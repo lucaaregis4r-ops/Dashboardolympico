36ff39d30d02f63eb1f977dae1f176938f793a2d
lucaaregis4r-ops <lucaaregis4r@gmail.com>
2026-08-06 16:52:25 -0300

    Add files via upload

---
+++ deployment-config.js
@@ -0,0 +1,7 @@
+window.OLYMPICO_CONFIG = Object.freeze({
+  staticHosting: true,
+  reportsEnabled: false,
+  athletesUrl: "./data/athletes.json",
+  physiotherapyUrl: "./data/physiotherapy.json",
+  generatedAt: "2026-08-06T18:40:26.373Z"
+});
